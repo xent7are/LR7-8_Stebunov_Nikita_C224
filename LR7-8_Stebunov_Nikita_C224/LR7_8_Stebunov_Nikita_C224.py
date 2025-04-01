8719c69c62b88ec485df8da21f804ad722e5c246
Stebunov Nikita <xent7are557@gmail.com>
2025-04-01 11:43:13 +0300

    Исправлено расположение точки останова программы pdb.set_trace().

--- LR7-8_Stebunov_Nikita_C224/LR7_8_Stebunov_Nikita_C224.py
+++ LR7-8_Stebunov_Nikita_C224/LR7_8_Stebunov_Nikita_C224.py
@@ -5,11 +5,11 @@ import pdb
 # Создание пустого словаря для хранения вопросов
 questions = {}
 
+# Установка точки останова для отладки с помощью pdb
+pdb.set_trace()
+
 mail = 'ADRESS'
 text = 'QUEST'
 
 # Добавление в словарь questions записи с ключом mail и значением text
 questions[mail] = text
-
-# Установка точки останова для отладки с помощью pdb
-pdb.set_trace()
